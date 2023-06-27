b1a260429832353e615cdce18014e8878b5ee416
Filip Was <fwas.corp@gmail.com>
2023-06-27 18:04:20 +0200

    first variables

--- js/wizard/steps/choose-fighter-step/choose-fighter-step.js
+++ js/wizard/steps/choose-fighter-step/choose-fighter-step.js
@@ -3,7 +3,14 @@ import { characterToggler } from './characterToggler.js';
 
 export const chooseFighterStep = () => {
    const chooseFighterStepWrapper = document.createElement('div');
-   chooseFighterStepWrapper.innerText = 'Choose fighter';
+
+   let topText = document.createElement('span');
+   let progressBar = document.createElement('img');
+   let characterImage = document.createElement('img');
+   let characterShadow = document.createElement('img');
+   let characterName = document.createElement('span');
+   // let leftArrow
+   // let rightArrow i'll be adding these arrows later
 
    let char = characterToggler(1);
 
